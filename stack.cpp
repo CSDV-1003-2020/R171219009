3bfdba0c370025d5c8711ec959742d09745d55c2
AYUSH BANSAL <bayush611@gmail.com>
2020-03-26 13:29:17 +0530

    stack using linklist

---
+++ stack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+struct Node { 
+   int data; 
+   struct Node *next; 
+}; 
+struct Node* top = NULL; 
+void push(int val) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node)); 
+   newnode->data = val; 
+   newnode->next = top; 
+   top = newnode; 
+}
+void pop() {
+   if(top==NULL)
+      cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< top->data <<endl;
+      top = top->next;
+   }
+}
+void display()
+int main()
