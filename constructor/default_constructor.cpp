8fca4393aa7a1274c0bfad31b1c9930cbe9dc092
AYUSH BANSAL <bayush611@gmail.com>
2020-03-30 23:51:13 +0530

    default construct called

---
+++ constructor/default_constructor.cpp
@@ -0,0 +1,19 @@
+// Cpp program to illustrate the 
+// concept of Constructors 
+#include <iostream> 
+using namespace std; 
+  
+class construct { 
+public: 
+    int a, b; 
+  
+    // Default Constructor 
+    construct() 
+    { 
+        a = 10; 
+        b = 20; 
+    } 
+}; 
+
+int main()
+
