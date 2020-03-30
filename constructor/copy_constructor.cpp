7de48235ee0ff9943ccea79b362ff42c9dd33c0a
AYUSH BANSAL <bayush611@gmail.com>
2020-03-30 23:54:06 +0530

    program added

---
+++ constructor/copy_constructor.cpp
@@ -0,0 +1,16 @@
+// Illustration 
+#include "iostream" 
+using namespace std; 
+  
+class point { 
+private: 
+  double x, y; 
+  
+public: 
+  // Non-default Constructor & default Constructor 
+  point (double px, double py) { 
+    x = px, y = py; 
+  } 
+}; 
+  
+int main(void) 
