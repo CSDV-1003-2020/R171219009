fbab62fd18fa084105e6a1898175b7855ef51048
AYUSH BANSAL <bayush611@gmail.com>
2020-03-31 15:15:47 +0530

    program started

---
+++ SCM--PROJECT/stack.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+struct Node { 
+   int data; 
+   struct Node *next; 
+}; 
+struct Node* top = NULL; 
+void push(int val) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node)); 
+   newnode->data = val; 
+   newnode->next = top; 
+   top = newnode; 
+}
